14b9d6cbe7d348c35bacf883aef6f368e1582490
Sousa <sousa.gates@gmail.com>
2024-10-06 15:04:55 -0300

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Moving between windows
+map("n", "<leader>k", ":wincmd k<CR>")
+map("n", "<leader>j", ":wincmd j<CR>")
+map("n", "<leader>h", ":wincmd h<CR>")
+map("n", "<leader>l", ":wincmd l<CR>")
+
+-- <leader>wv -> new vertical window
+map("n", "<leader>wv", ":vertical new<CR>", { noremap = true, silent = true })
+-- <leader>wh -> new horizontal window
+map("n", "<leader>wh", ":new<CR>", { noremap = true, silent = true })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,25 +15,6 @@ return {
 			---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-			highlight = {
-				enable = false,
-
-				-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-
 			indent = {
 				enable = true,
 
