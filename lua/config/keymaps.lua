38f45ea0f2e184e36b0fcc35c71558de8f9da7a4
Sousa <sousa.gates@gmail.com>
2024-10-14 18:10:11 -0300

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Leave insert mode with jj
+vim.keymap.set("i", "jj", "<Esc>")
+
 -- Moving between windows
 vim.keymap.set("n", "<leader>k", ":wincmd k<CR>")
 vim.keymap.set("n", "<leader>j", ":wincmd j<CR>")
